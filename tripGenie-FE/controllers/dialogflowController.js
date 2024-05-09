4cd3d80fad99dd9d0f2a263dd75eeb2e52237963
Aswin <aswinkulappada@gmail.com>
2024-05-10 01:03:45 +0530

    post

--- tripGenie-FE/controllers/dialogflowController.js
+++ tripGenie-FE/controllers/dialogflowController.js
@@ -7,11 +7,10 @@ export const handlePostRequest = (req, res) => {
 
   // Process the data (you can perform any logic here)
   console.log("Received POST data:", postData.destination);
-  const hotelResults = getHotels(postData.destination);
+  // const hotelResults = getHotels(postData.destination);
 
   // Send response
   res.status(200).json({
     message: "POST request received successfully",
-    data: hotelResults,
   });
 };
